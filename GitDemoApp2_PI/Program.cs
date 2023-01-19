dbda6596239d03da0e73cad56a550eead3e73caf
bhavana desai <bhavanadesai@gmail.com>
2023-01-19 12:47:35 +0530

    Add project files.

---
+++ GitDemoApp2_PI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoApp2_PI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing ...");
+            Console.ReadLine();
+        }
+    }
+}
